4808532016791d5fd693a70bf6ff2b9a646de82e
nirweb-ir <nirweb.ir@gmail.com>
2025-08-19 12:07:56 +0330

    1.0.2.1

--- server.js
+++ server.js
@@ -57,7 +57,20 @@ app.post('/forward-message', (req, res) => {
     res.status(404).send({ status: 'client discconnet message :', userId });
   }
 });
+app.post('/seen-message', (req, res) => {
+  const { userId } = req.body;
+  const socketId = clients[userId];
 
+  // console.log('get message:', JSON.stringify(req.body, null, 2));
+
+  if (socketId) {
+    // ارسال کل بدنه درخواست به کلاینت به صورت پیام 'new-message'
+    io.to(socketId).emit('seen-message', req.body);
+    res.send({ status: ' seen message to client : ', userId });
+  } else {
+    res.status(404).send({ status: 'client discconnet message :', userId });
+  }
+});
 
 server.listen(3000, () => {
   console.log('Socket.IO server is running on port 3000');
